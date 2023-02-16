e5cfe8a884da8ef3f457488b1776d3726027af30
Jamie Nichols <jamie337nichols@gmail.com>
2023-02-16 07:26:57 -0500

    updating test run

--- src/TESTS.js
+++ src/TESTS.js
@@ -1,1 +1,3 @@
+import Test from "./libs/client_unit_test/Test.js"
 import "./6502/__TEST__/CPU.js"
+Test.run()
--- src/main.js
+++ src/main.js
@@ -1,3 +1,1 @@
-import Test from "./libs/client_unit_test/Test.js"
 import "./TESTS.js"
-Test.run()
